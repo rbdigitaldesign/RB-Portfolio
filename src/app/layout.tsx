2cc1b614802b7cf20c2689e9d7ebef55043bbb07
Richard Bartlett <rbdigitaldesign11@gmail.com>
2025-08-18 07:29:41 +0000

    When the site is in light mode, can you ensure the text is accessible an

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,11 +33,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Merriweather:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
       <body
         className={cn(
           'min-h-screen bg-background font-body antialiased',
