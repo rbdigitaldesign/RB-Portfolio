8049291f3394d83636ba0708194468c917f57908
Richard Bartlett <rbdigitaldesign11@gmail.com>
2025-08-21 00:10:23 +0000

    Make site admin button #37605f

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "bg-[#37605f] text-primary-foreground hover:bg-[#37605f]/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         blogHome: "bg-[#37605f] text-primary-foreground hover:bg-accent hover:text-accent-foreground",
