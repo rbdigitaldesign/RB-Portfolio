93e07d508ea4ff3b3d84c596e9e3f02a065bcdc3
Richard Bartlett <rbdigitaldesign11@gmail.com>
2025-08-26 11:44:25 +0000

    Great, I've added all the files and mainfest docs into the public and ap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="https://i.imgur.com/zx6J4dX.png" type="image/png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2f4f4f" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="apple-mobile-web-app-title" content="RB Digital" />
       </head>
       <body
         className={cn(
