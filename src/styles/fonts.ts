61a4265c12be12b2b29d2c78a12b67c075a52d4d
Richard Bartlett <rbdigitaldesign11@gmail.com>
2025-08-28 14:19:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -4,7 +4,8 @@ import localFont from "next/font/local";
 
 export const ananias = localFont({
   src: [
-    { path: "../../public/fonts/ananias.woff2", weight: "400", style: "normal" },
+    { path: "../../public/fonts/Ananias.ttf", weight: "400", style: "normal" },
+    { path: "../../public/fonts/Ananias Bold.ttf", weight: "700", style: "normal" },
   ],
   variable: "--font-ananias",
   display: "swap",
